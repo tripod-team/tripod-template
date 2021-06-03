5b19882fdb55def12488362ab1433ecc366ccc3c
zhaobaojue <zhaobaojue@gmail.com>
2021-06-03 11:22:56 +0800

    file: rename babel.config.js to babel.js

--- config/babelLoader.js
+++ config/babelLoader.js
@@ -1,9 +1,11 @@
 module.exports = (config, resolve) => {
   const baseRule = config.module.rule('js').test(/.(j|t)sx?$/);
-  const babelPath = resolve('babel.config.js');
+  const babelPath = resolve('babel.js');
   const babelConf = require(babelPath);
   const version = require(resolve('node_modules/@babel/core/package.json'))
     .version;
+
+  console.log(babelConf());
   
   return () => {
     baseRule
