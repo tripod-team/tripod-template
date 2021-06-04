b84e39588f4a1ebcb8f245236927f4a1cea5fb24
zhaobaojue <zhaobaojue@gmail.com>
2021-06-04 10:55:01 +0800

    fix: babel-loader set exclude node_modules to fix __webpack_require__ is not a function

--- config/babelLoader.js
+++ config/babelLoader.js
@@ -1,5 +1,5 @@
 module.exports = (config, resolve) => {
-  const baseRule = config.module.rule('js').test(/.(j|t)sx?$/);
+  const baseRule = config.module.rule('js').test(/.(j|t)sx?$/).set('exclude', /node_modules/);
   const babelPath = resolve('babel.js');
   const babelConf = require(babelPath);
   const version = require(resolve('node_modules/@babel/core/package.json'))
