82db207008c10081e42e747dcde4f7e7d29164c6
zhaobaojue <zhaobaojue@gmail.com>
2021-11-29 11:43:10 +0800

    config: fix local yarn link resolve problem

--- config/resolve.js
+++ config/resolve.js
@@ -2,6 +2,10 @@ module.exports = (config, resolve) => {
   return () => {
     config.resolve.extensions.add('.js').add('.jsx').add('.css').end();
 
+    config.resolve.modules
+      .add('node_modules')
+      .add(resolve('node_modules'));
+
     config.resolve.alias
       .set('@', resolve('src'))
       .set('views', resolve('src/views'))
